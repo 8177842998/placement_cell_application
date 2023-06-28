0997564d42bae4dbd5a781b7b9b647891198b110
My Name <90561078+8177842998@users.noreply.github.com>
2023-06-29 01:25:11 +0530

    new

--- config/mongoose.js
+++ config/mongoose.js
@@ -1,50 +1,49 @@
-<<<<<<< HEAD
-// requiring the library
-const mongoose = require("mongoose");
+// <<<<<<< HEAD
+// // requiring the library
+// const mongoose = require("mongoose");
 
-//connecting to the database
-mongoose.connect(
-  'mongodb://127.0.0.1:27017/passport'
-  // `mongodb://localhost:27017/placement-cell`
-  // "mongodb+srv://placed:vishwa40@cluster0.k4ug4kl.mongodb.net/Student_data?retryWrites=true&w=majority"
-  // "mongodb+srv://Vishwajit:1pUFSRGhfU3tWOMC@cluster1.ftgiqi0.mongodb.net/Student_data?retryWrites=true&w=majority"
-  );
+// //connecting to the database
+// // mongoose.connect(
+//   'mongodb://127.0.0.1:27017/passport'
+//   // `mongodb://localhost:27017/placement-cell`
+//   // "mongodb+srv://placed:vishwa40@cluster0.k4ug4kl.mongodb.net/Student_data?retryWrites=true&w=majority"
+//   // "mongodb+srv://Vishwajit:1pUFSRGhfU3tWOMC@cluster1.ftgiqi0.mongodb.net/Student_data?retryWrites=true&w=majority"
+//   );
 
-// acquiring the connection to check if it is succesfull
-const db = mongoose.connection;
+// // acquiring the connection to check if it is succesfull
+// const db = mongoose.connection;
 
-// checking for the error
-db.on("error", console.error.bind(console, "error in connecting the database"));
+// // checking for the error
+// db.on("error", console.error.bind(console, "error in connecting the database"));
 
-// up and running then print the statement
-db.once("open", () => {
-  console.log("succesfully connected to database");
-});
+// // up and running then print the statement
+// db.once("open", () => {
+//   console.log("succesfully connected to database");
+// });
 
-// exporting the connection
-=======
-// requiring the library
-const mongoose = require("mongoose");
+// // exporting the connection
+// // requiring the library
+// const mongoose = require("mongoose");
 
-//connecting to the database
-mongoose.connect(
-  'mongodb://127.0.0.1:27017/passport'
-  // `mongodb://localhost:27017/placement-cell`
-  // "mongodb+srv://placed:vishwa40@cluster0.k4ug4kl.mongodb.net/Student_data?retryWrites=true&w=majority"
-  // "mongodb+srv://Vishwajit:1pUFSRGhfU3tWOMC@cluster1.ftgiqi0.mongodb.net/Student_data?retryWrites=true&w=majority"
-  );
+// //connecting to the database
+// mongoose.connect(
+//   'mongodb://127.0.0.1:27017/passport'
+//   // `mongodb://localhost:27017/placement-cell`
+//   // "mongodb+srv://placed:vishwa40@cluster0.k4ug4kl.mongodb.net/Student_data?retryWrites=true&w=majority"
+//   // "mongodb+srv://Vishwajit:1pUFSRGhfU3tWOMC@cluster1.ftgiqi0.mongodb.net/Student_data?retryWrites=true&w=majority"
+//   );
 
-// acquiring the connection to check if it is succesfull
-const db = mongoose.connection;
+// // acquiring the connection to check if it is succesfull
+// const db = mongoose.connection;
 
-// checking for the error
-db.on("error", console.error.bind(console, "error in connecting the database"));
+// // checking for the error
+// db.on("error", console.error.bind(console, "error in connecting the database"));
 
-// up and running then print the statement
-db.once("open", () => {
-  console.log("succesfully connected to database");
-});
+// // up and running then print the statement
+// db.once("open", () => {
+//   console.log("succesfully connected to database");
+// });
 
-// exporting the connection
->>>>>>> 833b6f12c5ba070e3037a960533adea02ad5f620
-module.exports = db;
+// // exporting the connection
+// >>>>>>> 833b6f12c5ba070e3037a960533adea02ad5f620
+// module.exports = db;
