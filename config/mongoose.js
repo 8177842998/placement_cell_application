5092a4c71de247a2ee6fb0f7e1822cd670f76a6c
Divya Patne <90561078+8177842998@users.noreply.github.com>
2023-06-29 00:46:40 +0530

    a

---
+++ config/mongoose.js
@@ -0,0 +1,24 @@
+// requiring the library
+const mongoose = require("mongoose");
+
+//connecting to the database
+mongoose.connect(
+  'mongodb://127.0.0.1:27017/passport'
+  // `mongodb://localhost:27017/placement-cell`
+  // "mongodb+srv://placed:vishwa40@cluster0.k4ug4kl.mongodb.net/Student_data?retryWrites=true&w=majority"
+  // "mongodb+srv://Vishwajit:1pUFSRGhfU3tWOMC@cluster1.ftgiqi0.mongodb.net/Student_data?retryWrites=true&w=majority"
+  );
+
+// acquiring the connection to check if it is succesfull
+const db = mongoose.connection;
+
+// checking for the error
+db.on("error", console.error.bind(console, "error in connecting the database"));
+
+// up and running then print the statement
+db.once("open", () => {
+  console.log("succesfully connected to database");
+});
+
+// exporting the connection
+module.exports = db;
